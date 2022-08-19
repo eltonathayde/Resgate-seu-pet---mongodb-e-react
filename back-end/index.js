5576d081e72f3808e5070c1017d986c494da59aa
elton lima <eltonathayde2@gmail.com>
2022-08-19 16:15:14 -0300

    setup inical do projeto adote seu pet

---
+++ back-end/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const  cors = require('cors')
+
+
+const app = express()
+
+//  configurando resposta Json
+
+app.use(express.json())
+
+
+// resolvendo problema de cors 
+
+app.use(cors({credentials:true, origin:'http://localhost:3000'}))
+
+//  pasta publica  para imagens  
+
+app.use(express.static('public'))
+
+// Routes
+
+app.listen(5000)
