e23d50097576057123a0dc62a7720e7a04db3787
elton lima <eltonathayde2@gmail.com>
2022-08-22 08:55:18 -0300

    Rotas do usuario e controller do usuario inicados

---
+++ back-end/controllers/UserController.js
@@ -0,0 +1,11 @@
+const User = require('../models/User')
+
+
+
+module.exports = class UserController {
+
+static async register(req,res) {
+    res.json('Óla GEt A PEt')
+}
+
+}
--- back-end/db/conn.js
+++ back-end/db/conn.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongooose')
+const mongoose = require('mongoose')
 
 async function main(){
     await mongoose.connect('mongodb://localhost:27017/getapet')
